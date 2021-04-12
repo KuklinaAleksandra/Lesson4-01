fb85281824d4e539bd4914471c98eb49ba4c9de6
Kuklina <kuklina.aleksandra@rambler.ru>
2021-04-12 20:18:01 +0500

    Добавьте файлы проекта.

---
+++ Lesson4-01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lesson4_01
+{
+    using System;
+
+    namespace Lesson4_1
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Console.WriteLine(GetFullName("Александр", "Сергеевич", "Пушкин"));
+                Console.WriteLine(GetFullName("Лев", "Николаевич", "Толстой"));
+                Console.WriteLine(GetFullName("Антон", "Павлович", "Чехов"));
+
+                Console.WriteLine();
+            }
+
+            static string GetFullName(string firstName, string patronymic, string lastName)
+            {
+
+
+                return $"{lastName} {firstName} {patronymic}";
+            }
+
+        }
+    }
+
+}
